518db894bf85f278fb90aeeba2a08b8988cf2f77
Nurudeen <111026445+Nusultkang1@users.noreply.github.com>
2022-09-13 16:19:00 -0700

    Task 0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,22 +6,13 @@
  * Return: void
  */
 
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-
-	return (0);
-}
-
 void print_alphabet(void)
 {
 	int n;
-
+	
 	for (n = 'a'; n <= 'z'; n++)
 	{
 		_putchar(n);
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
