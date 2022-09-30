9d073d06647f115bbef95b43ef4346df8feeb988
Nusultkang1 <nurudeensultan28@gmail.com>
2022-10-01 00:56:07 +0100

    Task

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - function checks for uppercase character
+ * @c: the character set to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
