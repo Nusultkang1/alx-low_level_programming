6909afe731d2acee71935c7db0c02272ea34e1bb
Nurudeen <nurudeensultan28@gmail.com>
2023-04-17 02:33:04 +0100

    Retrial with binary search algo 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,4 +1,36 @@
 #include "search_algos.h"
+/**
+ * print_array - prints an array of integers
+ * @array: pointer to the first element of the array to be printed
+ * @size: size of the array.
+ *
+ * Return: nothing.
+ */
+
+void print_array(int *array, int size)
+{
+	int x;
+
+	if (array == NULL || size == 0)
+		return;
+
+	printf("Searching in array: ");
+
+	for (x = 0; x < size; x++)
+	{
+		if (x == 0)
+		{
+			printf("%d", array[x]);
+		}
+		else
+		{
+			printf(", %d", array[x]);
+		}
+
+	}
+
+	printf("\n");
+}
 
 /**
  * binary_search - Performs a binary search on an array of integers.
@@ -14,25 +46,27 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t y, laptop, bottle;
+	int left = 0;
+	int right = size - 1;
+	int mid;
 
-	if (array == NULL)
+	if (array == NULL || size == 0 || array[0] > value)
 		return (-1);
 
-for (laptop = 0, bottle = size - 1; laptop >= bottle;)
+	while (left <= right)
 	{
-		printf("Searching in array: ");
-		for (y = laptop; y < bottle; y++)
-			printf("%d, ", array[y]);
-		printf("%d\n", array[y]);
+		mid = (left + right) / 2;
 
-		y = laptop + (bottle - laptop) / 2;
-		if (array[i] == value)
-			return (y);
-		if (array[i] > value)
-			bottle = y - 1;
+		print_array(&array[left], (right - left) + 1);
+
+		if (array[mid] == value)
+			return (mid);
+
+		if (array[mid] > value)
+			right = mid - 1;
+
 		else
-			laptop = y + 1;
+			left = mid + 1;
 	}
 
 	return (-1);
